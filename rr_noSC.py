fb423dc0cd29f7be314e5bc8bbfb27ed3d94d4c2
Jeffrey Larson <jmlarson@anl.gov>
2023-05-11 07:10:01 -0500

    black files

--- rr_noSC.py
+++ rr_noSC.py
@@ -1,16 +1,15 @@
-
-#from mpi4py import MPI
+# from mpi4py import MPI
 import numpy as np
 import synergia
 
 import rr_setup
 
-#from rr_options import opts
+# from rr_options import opts
+
 
 def print_statistics(bunch):
-
     parts = bunch.get_particles_numpy()
-    print(parts.shape,  ", ", parts.size )
+    print(parts.shape, ", ", parts.size)
     print("shape: {0}, {1}".format(parts.shape[0], parts.shape[1]))
 
     mean = synergia.bunch.Core_diagnostics.calculate_mean(bunch)
@@ -20,39 +19,38 @@ def print_statistics(bunch):
 
 
 def get_lattice_rr():
-    
     lattice_file = open("rr_tuned.json", "r")
     lattice = synergia.lattice.Lattice.load_from_json(lattice_file.read())
 
     lattice.set_all_string_attribute("extractor_type", "libff")
-    #synergia.simulation.Lattice_simulator.tune_circular_lattice(lattice)                                 
+    # synergia.simulation.Lattice_simulator.tune_circular_lattice(lattice)
     return lattice
 
-def run_rr():
 
+def run_rr():
     # Run Recyler setup in order to tune lattice
     # Run rr_setup to get covariance matrix in order to populate bunches
     rr_setup.setup()
 
     # Define basic parameters for the simulation
-    real_particles = 1e10 #Number of real particles in simulation
+    real_particles = 1e10  # Number of real particles in simulation
     refine = 1
-    gridx = 32*refine
-    gridy = 32*refine
-    gridz = 128*refine
-    partpercell = 8 #Number of particles per cell
-    num_bunches = 1 #Number of bunches to simulate
+    gridx = 32 * refine
+    gridy = 32 * refine
+    gridz = 128 * refine
+    partpercell = 8  # Number of particles per cell
+    num_bunches = 1  # Number of bunches to simulate
 
     grid = [gridx, gridy, gridz]
-    print('Grid Size:')
+    print("Grid Size:")
     print(grid)
     macro_particles = gridx * gridy * gridz * partpercell
-    print('Number of macroparticles:')
+    print("Number of macroparticles:")
     print(macro_particles)
-    #macro_particles = 100000 #Number of macroparticles to simulate
-    spacing = 5.64526987439 #Bucket length calculated from synergia2
+    # macro_particles = 100000 #Number of macroparticles to simulate
+    spacing = 5.64526987439  # Bucket length calculated from synergia2
 
-    seed = 4 # Random seed to use
+    seed = 4  # Random seed to use
 
     # Get tuned Recycler lattice and reference particle for lattice
     lattice = get_lattice_rr()
@@ -60,7 +58,8 @@ def run_rr():
 
     # Initiate bunch simulator (For now single bunch)
     sim = synergia.simulation.Bunch_simulator.create_bunch_train_simulator(
-        ref_part, macro_particles, real_particles, num_bunches, spacing)
+        ref_part, macro_particles, real_particles, num_bunches, spacing
+    )
 
     # Enforce longitudinal bucket conditions (Mandatory if RF is turned on)
     sim.set_longitudinal_boundary(synergia.bunch.LongitudinalBoundary.periodic, spacing)
@@ -69,7 +68,7 @@ def run_rr():
     # Can introduce horizontal or vertical kick
     xkick = 0
     ykick = 0
-    means = np.zeros([6], 'd')+np.array([0,xkick,0,ykick,0,0])
+    means = np.zeros([6], "d") + np.array([0, xkick, 0, ykick, 0, 0])
     covars = np.load("correlation_matrix.npy")[()]
 
     # Initiate parallel communication protocol
@@ -82,13 +81,13 @@ def run_rr():
     # Populate according to means and covariance parameters
 
     for b in range(num_bunches):
-        bunch = sim.get_bunch(0, b) # Get b-th bunch at 0-th train
+        bunch = sim.get_bunch(0, b)  # Get b-th bunch at 0-th train
         synergia.bunch.populate_6d(dist, bunch, means, covars)
         print_statistics(bunch)
 
     # Define space-charge operator (For now no space charge)
-    steps = 416 # Number of steps to use for stepper (What does this mean?)
-    sc_ops = synergia.collective.Dummy_CO_options() # No space-charge option
+    steps = 416  # Number of steps to use for stepper (What does this mean?)
+    sc_ops = synergia.collective.Dummy_CO_options()  # No space-charge option
     stepper = synergia.simulation.Split_operator_stepper(sc_ops, steps)
 
     # Define propagator for simulations
@@ -98,10 +97,10 @@ def run_rr():
     # For now keep options to single bunch
 
     # Track individual particles and save to individual files
-    part_track = 1000 # Number of particles to track
-    nturns_track = 1 # Number of turns between each tracking 
+    part_track = 1000  # Number of particles to track
+    nturns_track = 1  # Number of turns between each tracking
     diag_part = synergia.bunch.Diagnostics_particles("diag_particles.h5", part_track)
-    sim.reg_diag_per_turn(diag_part, period = nturns_track)
+    sim.reg_diag_per_turn(diag_part, period=nturns_track)
 
     # Get mean diagnostics for single bunch at every turn
     diag_full2 = synergia.bunch.Diagnostics_full2("diag_full.h5")
@@ -124,10 +123,9 @@ def run_rr():
 
 
 def main():
-
     print("Running Recycler Ring Simulation (No Space Charge)")
     run_rr()
 
-    
+
 # Run simulation
 main()
--- rrnova_qt60x.py
+++ rrnova_qt60x.py
@@ -1,29 +1,34 @@
 import numpy as np
 import re
+
 # get focussing and defocussing trim quads in the 600 section
 # for adjusting tmunes.
 # Information from Rob Ainsworth 2016-03-17
 
+
 # Phase trombone coefficients from Meiqin Xiao 2016-03-18
 def get_rr60_trombone_settings(delta_nux, delta_nuy):
     # this is the matrix of coefficients from Meiqin
-    trombone_coeffs = np.array([
-[-136.992, 1678.1, 144.639, -1696.96, -74.2027, 1786.81, 47.9609, -1762.77, -52.5754], 
-[35.2887, 75.3339, -33.5186, -44.1195,37.1867, 42.1706, -35.7823, -14.7396, 37.5639],
-[5756.82, -1306.98, -3853.78, 1679.54, 3048.33, -1551.15, -795.505, 1760.18, 268.669],
-[-151.905, -22.4222, 164.601, 12.7766, -181.884, -12.9934, 186.623, 6.74839, -192.135],
-[2.98716, 15.3422, 2.88162, 14.9116, 3.0027, 15.567, 2.86146, 15.0054, 3.01143],
-[-15.6529, -2.77037, -14.7677, -2.94121, -15.5028, -2.74323, -14.9448, -2.96573, -15.3222]])
-    b  = np.array([0.00711792, 0.00276458, 0.0249164, 0.00257196, delta_nux, delta_nuy])
-    #elements of kcoeff will be k1 values for qt601..qt609
+    trombone_coeffs = np.array(
+        [
+            [-136.992, 1678.1, 144.639, -1696.96, -74.2027, 1786.81, 47.9609, -1762.77, -52.5754],
+            [35.2887, 75.3339, -33.5186, -44.1195, 37.1867, 42.1706, -35.7823, -14.7396, 37.5639],
+            [5756.82, -1306.98, -3853.78, 1679.54, 3048.33, -1551.15, -795.505, 1760.18, 268.669],
+            [-151.905, -22.4222, 164.601, 12.7766, -181.884, -12.9934, 186.623, 6.74839, -192.135],
+            [2.98716, 15.3422, 2.88162, 14.9116, 3.0027, 15.567, 2.86146, 15.0054, 3.01143],
+            [-15.6529, -2.77037, -14.7677, -2.94121, -15.5028, -2.74323, -14.9448, -2.96573, -15.3222],
+        ]
+    )
+    b = np.array([0.00711792, 0.00276458, 0.0249164, 0.00257196, delta_nux, delta_nuy])
+    # elements of kcoeff will be k1 values for qt601..qt609
     kcoeff = np.linalg.lstsq(trombone_coeffs, b, rcond=None)
     return kcoeff[0]
-    
+
 
 def adjust_rr60_trim_quads(lattice, delta_nux, delta_nuy):
     # the k coefficients are actually k*L so I have to divide by
     # l(0.3048)
-    kcoeff = get_rr60_trombone_settings(delta_nux, delta_nuy)/0.3048
+    kcoeff = get_rr60_trombone_settings(delta_nux, delta_nuy) / 0.3048
     valid_trim_name_regex = "qt60([1-9])[a-d]"
     trimobj = re.compile(valid_trim_name_regex)
     for elem in lattice.get_elements():
@@ -32,12 +37,15 @@ def adjust_rr60_trim_quads(lattice, delta_nux, delta_nuy):
             mo = trimobj.match(elem.get_name())
             if mo:
                 quadnum = int(mo.group(1))
-                elem.set_double_attribute("k1", kcoeff[quadnum-1])
+                elem.set_double_attribute("k1", kcoeff[quadnum - 1])
 
 
 if __name__ == "__main__":
     import synergia
-    lattice = synergia.lattice.Mad8_reader().get_lattice("ring605_fodo", "../rrnova_one_bunch/RRNOvAMu2E_04272015_flat_fixed.lat")
+
+    lattice = synergia.lattice.Mad8_reader().get_lattice(
+        "ring605_fodo", "../rrnova_one_bunch/RRNOvAMu2E_04272015_flat_fixed.lat"
+    )
     print("read lattice: ", len(lattice.get_elements()), " elements")
     kcoeff = adjust_rr60_trim_quads(lattice, 0.05, 0.05)
-    print ("new k coefficients for magnets: ", kcoeff)
+    print("new k coefficients for magnets: ", kcoeff)
